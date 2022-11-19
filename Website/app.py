965e265fe70a54fa612e005488a23e56eaaa15a0
Heinzino <51274202+Heinzino@users.noreply.github.com>
2022-11-19 12:45:21 -0700

    init style, research

--- Website/app.py
+++ Website/app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def layout():
     return render_template("layout.html")
+
+@app.route("/research", methods=["GET","POST"])
+def research():
+    return render_template("research.html")

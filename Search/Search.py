85b17f944fc1c312185d1f496dc4114782efd4b1
Aaron-Giang <aarongiang88@hotmail.com>
2022-11-19 18:40:29 -0700

    changed search

--- Search/Search.py
+++ Search/Search.py
@@ -4,27 +4,46 @@ except ImportError:
     print("No module named 'google' found")
 
 
+"""
+general resources:
+List 1: food banks
+List 2: Charities 
+
+List 3: job postings in the area
+List 4: career workshops in the area
+
+academic resources:
+List 5:scholarships and bursaries in that area
+List 6:tutoring workshops and studying workshops
+
+mental health resources:
+List 7:Mental heath support in area
+List 8:suicide helpline in the area
+"""
 
 def datafinder(country,city,num):
-    queryScolorShip = "Scholoarships in %s %s"%(country,city)
-    queryMentalHealth = "Mental health support centers in %s %s"%(country,city)
+    queries= [
+    "Food banks in  %s %s" %(country,city),
+    "Charities in  %s %s" %(country,city),
+    "Job postings in  %s %s" %(country,city),
+    "Career workshops in  %s %s" %(country,city),
+    "Scholarships and bursaries in %s %s" %(country,city),
+    "Tutoring workshops and studying workshops in %s %s" %(country,city),
+    "Mental health support centers in %s %s" %(country,city),
+    "Suicide helpline in  %s %s" %(country,city),
+    ]
+    listsOfSite = []
 
-    listsOfSite = [[],[],[]]
-
-    for x in search(queryScolorShip,num=num,stop=num,pause=2):
-        listsOfSite[0].append(x)
-    for y in search(queryMentalHealth,num=num,stop=num,pause=2):
-        listsOfSite[1].append(y)
-
+    for x in queries:
+        tempList = []
+        for y in search(x,num=num,stop = num,pause =2):
+            tempList.append(y)
+        listsOfSite.append(tempList)
     return listsOfSite
 
     
-data = datafinder("canada","calgary",10)
+data = datafinder("canada","calgary",3)
 
-print("scholorship")
-for x in data[0]:
+for x in data:
     print(x)
-
-print("Mental health")
-for x in data[1]:
-    print(x)
+    print("------------------------------------")
